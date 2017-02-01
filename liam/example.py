f7b225dc0f96f42da66c94ed6c6bd5700a1a26b7
Bruce Stringer <bruce.stringer@rackspace.com>
2017-02-01 11:30:42 -0600

    Update examples.py to use new scanning features

--- liam/example.py
+++ liam/example.py
@@ -7,10 +7,27 @@ CREDS = {
 }
 
 
-def main():
-    scanner = liam.Scanner(CREDS)
-    results = scanner.full_scan()
+def example_1():
+    # run a scan of ec2 subnets in us-east regions, returning arns
+    scanner = liam.Scanner(
+        CREDS,
+        services=['ec2'],
+        regions=['us-east-1', 'us-east-2'],
+        collections=['subnets']
+    )
+    results = scanner.scan(return_arns=True)
     print(results)
+
+
+def example_2():
+    # run a full scan of everything
+    scanner = liam.Scanner(CREDS)
+    results = scanner.scan()
+    print(results)
+
+
+def main():
+    example_1()
 
 
 if __name__ == '__main__':
